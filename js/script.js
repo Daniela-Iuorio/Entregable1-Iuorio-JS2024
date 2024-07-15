ae3cb27b4eb20bb95b9b2b4d04ef75ede11435a0
Daniela Iuorio <lic.iuorio.daniela@gmail.com>
2024-07-15 00:25:44 -0300

    Uso ejemplo de semana 3 y adapto

--- js/script.js
+++ js/script.js
@@ -1,155 +1,111 @@
-// // //Pido info al usuario, lo saludo y pido confirmación de que quiere jugar
-// let nombre = prompt('Ingrese su nombre')
-// let genero = prompt('Ingrese F para femenino, M para masculino o x para otro').toLowerCase()
-// console.log('Nombre: '+nombre+' Género: '+genero)
-// switch(genero){
-//     case 'f':
-//         alert('Bienvenida, '+ nombre +', al juego de los números!')
-//         break
-//     case 'm':
-//         alert('Bienvenido, '+ nombre +', al juego de los números!')
-//         break
-//     case 'x':
-//         alert('Bienvenid@, '+ nombre +', al juego de los números!')
-//         break
-//     default:
-//         alert('Ha ingresado una opción inválida de género. Saludos! Continuemos al juego.')
-// }
+//Valores de cursos y material didáctico
 
-// confirm ('Este juego consiste en adivinar un número de 3 dígitos diferentes en menos de 10 intentos. Yo te diré qué dígitos has adivinado y cuáles son incorrectos. ¿Comenzamos?')
+let elementary = 1000
+let intermediate = 1500
+let advanced = 2000
+let business = 3000
+let technical = 2500
+let exams = 2500
+let libroCurso = 300
+let audioLibroCurso = 150
+let libroPractica = 200
 
-// //Genero el número que se debe adivinar. Creo un array y muestro el número en consola.
+//solicitar información al usuario y ejecutar funciones correspondientes según el caso (alumno o profe)
+let usuario = prompt('Are you a student or a teacher? Enter S for student or T for teacher').toLowerCase()
 
-// let numeroAleatorio1 = Math.floor(Math.random() * 10)
-
-// let numeroAleatorio2
-// do {
-//   numeroAleatorio2 = Math.floor(Math.random() * 10)
-// } while (numeroAleatorio2 === numeroAleatorio1)
-
-// let numeroAleatorio3
-// do {
-//   numeroAleatorio3 = Math.floor(Math.random() * 10)
-// } while (numeroAleatorio3 === numeroAleatorio1 || numeroAleatorio3 === numeroAleatorio2)
-
-// const numeroSecreto = [numeroAleatorio1, numeroAleatorio2, numeroAleatorio3]
-
-// console.log('El número aleatorio generado es: '+numeroSecreto.join(' ')
-
-// )
-
-
-// //Creo funciones para comparar cada variable correspondiente a un dígito del número aleatorio generado.
-
-// let digito1, digito2, digito3
-
-// function compararDigito1 (){
-//     if(digito1 === numeroAleatorio1){
-//         return ('CORRECTO.')
-//     }else{
-//         return('INCORRECTO.')
-//     }
-// }
-
-// function compararDigito2 (){
-//     if(digito2 === numeroAleatorio2){
-//         return ('CORRECTO.')
-//     }else{
-//         return('INCORRECTO.')
-//     }
-// }
-
-// function compararDigito3 (){
-//     if(digito3 === numeroAleatorio3){
-//         return ('CORRECTO.')
-//     }else{
-//         return('INCORRECTO.')
-//     }
-// }
-
-
-// // //Creo un ciclo con un máximo de 10 intentos que pida al usuario ingresar 3 dígitos. Cotejo y devuelvo info llamando a las funciones. Repito ciclo hasta que los tres dígitos coincidan o hasta que se agoten los intentos.
-
-// let intentosAgotados = true 
-
-// for (let i=0; i<10; i++) {
-
-//         digito1 = parseInt(prompt('Ingrese el primer dígito'))
-//         digito2 = parseInt(prompt('Ingrese el segundo dígito'))
-//         digito3 = parseInt(prompt('Ingrese el tercer dígito'))
-//         const numeroIngresado = [digito1, digito2, digito3]
-//         console.log('El número ingresado es: '+numeroIngresado.join(' '))
-//         alert('El número ingresado es: '+numeroIngresado.join(' '))
-//         compararDigito1()
-//         compararDigito2()
-//         compararDigito3()
-//         alert('\nEl primer dígito ('+digito1+') es '+compararDigito1()+'\nEl segundo dígito ('+digito2+') es '+compararDigito2()+'\nEl tercer dígito ('+digito3+') es '+compararDigito3())
-
-//     if(digito1 == numeroAleatorio1 && digito2 == numeroAleatorio2 && digito3 == numeroAleatorio3){
-//         alert('¡Felicitaciones! \nHas adivinado el número en 10 intentos o menos. \nPuedes volver a jugar si reinicias la página.')
-//         intentosAgotados = false
-//         break
-//     }
-// }
-
-// if(intentosAgotados){
-//     alert('Lo siento, se acabaron los intentos disponibles. \nEl número era: '+numeroSecreto.join(' ')+'. \nSi lo deseas, puedes reiniciar la página y volver a intentarlo.')
-// }
-
-function generarNumeroAleatorio() {
-    let numero = '';
-    while (numero.length < 3) {
-        let digito = Math.floor(Math.random() * 10);
-        if (!numero.includes(digito.toString())) {
-            numero += digito.toString();
-        }
-    }
-    return numero;
+if (usuario == s) {
+    redirectStudent()
+} else if (usuario == t) {
+    redirectTeacher()
+} else {
+    alert('Invalid. Please refresh the page and try again.')
 }
 
-function iniciarJuego() {
-    const numeroSecreto = generarNumeroAleatorio();
-    let intentos = 0;
-    const maxIntentos = 15;
-    let juegoTerminado = false;
+//Sección ALUMNOS
+function redirectStudent() {
+    alert("You are being redirected to the Student's section")
 
-    while (intentos < maxIntentos && !juegoTerminado) {
-        let intentoUsuario = prompt(`Intenta adivinar el número de 3 dígitos (Intento ${intentos + 1} de ${maxIntentos}):`);
+    let studentName = prompt("What's your name?")
 
-        if (intentoUsuario === null) {
-            alert("Juego cancelado.");
-            return;
-        }
+    alert('Hello, '+studentName+'. Welcome to English Connection!')
 
-        if (intentoUsuario.length !== 3 || isNaN(intentoUsuario) || new Set(intentoUsuario).size !== 3) {
-            alert("Ingresa un número de 3 dígitos diferentes.");
-            continue;
-        }
+    let contadorTotal = 0
+    let listadoProductos = "Listado de productos:"
 
-        intentos++;
-        let correctos = 0;
-        let pistas = [];
+    let cursoElegido = prompt('Ingrese el código del curso al que desea anotarse: \n00 - Elementary \n01 - Intermediate \n02 - Advanced \n03 - Business English \n04 - Technical English \n05 - Exam Training')
 
-        for (let i = 0; i < 3; i++) {
-            if (intentoUsuario[i] === numeroSecreto[i]) {
-                correctos++;
-                pistas.push(`${intentoUsuario[i]}: Correcto`);
-            } else if (numeroSecreto.includes(intentoUsuario[i])) {
-                pistas.push(`${intentoUsuario[i]}: Incorrecto pero presente`);
-            } else {
-                pistas.push(`${intentoUsuario[i]}: Incorrecto`);
+    switch (cursoElegido) {
+        case '00':
+            alert('Ha elegido el curso Elementary por un valor de $' + elementary + '.')
+            contadorTotal += elementary
+            listadoProductos += '\nCurso Elementary $'+elementary
+            break
+        case '01':
+            alert('Ha elegido el curso Intermediate por un valor de $' + intermediate + '.')
+            contadorTotal += intermediate
+            listadoProductos += '\nCurso Intermediate $'+intermediate
+            break
+        case '02':
+            alert('Ha elegido el curso Advanced por un valor de $' + advanced + '.')
+            contadorTotal += advanced
+            listadoProductos += '\nCurso Advanced $'+advanced
+            break
+        case '03':
+            alert('Ha elegido el curso Business English por un valor de $' + business + '.')
+            contadorTotal += business
+            listadoProductos += '\nCurso Business English $'+business
+            break
+        case '04':
+            alert('Ha elegido el curso Technical English por un valor de $' + technical + '.')
+            contadorTotal += technical
+            listadoProductos += '\nCurso Technical English $'+technical
+            break
+        case '05':
+            alert('Ha elegido el curso Exam Training por un valor de $' + exams + '.')
+            contadorTotal += exams
+            listadoProductos += '\nCurso Exam Training $'+exams
+            break
+    }
+
+    let compraMaterial = confirm('¿Desea adquirir el material didáctico del curso en formato digital?')
+
+    if (compraMaterial) {
+        while(compraMaterial) {
+            let materialElegido = prompt('Ingrese el código del producto que desea adquirir. \n06 - Libro de Curso \n07 - Audio del libro de curso \n08 - Libro de Practica')
+
+            switch (materialElegido) {
+                case '06':
+                    alert('Ha elegido el Libro de Curso por un valor de $' + libroCurso + '.')
+                    contadorTotal += libroCurso
+                    listadoProductos += '\nLibro de Curso $'+libroCurso
+                    break
+                case '07':
+                    alert('Ha elegido el Audio del libro de curso por un valor de $' + audioLibroCurso + '.')
+                    contadorTotal += audioLibroCurso
+                    listadoProductos += '\nAudio de Libro de Curso $'+audioLibroCurso
+                    break
+                case '08':
+                    alert('Ha elegido el Libro de Práctica por un valor de $' + libroPractica + '.')
+                    contadorTotal += libroPractica
+                    listadoProductos += '\nLibro de Práctica $'+libroPractica
+                    break
             }
-        }
 
-        alert(`Pistas:\n${pistas.join('\n')}`);
-
-        if (correctos === 3) {
-            alert(`¡Felicidades! Has adivinado el número ${numeroSecreto} en ${intentos} intentos.`);
-            juegoTerminado = true;
-        } else if (intentos === maxIntentos) {
-            alert(`Lo siento, has agotado los ${maxIntentos} intentos. El número era ${numeroSecreto}.`);
+            compraMaterial = confirm('¿Desea agregar más productos?')
         }
     }
+
+    alert(listadoProductos +'\nEl total a pagar es de $'+contadorTotal+'. Thank you!')
+
+
 }
 
-iniciarJuego();
+//Sección PROFESORES
+function redirectTeacher() {
+    alert("You are being redirected to the Teacher's section")
+
+    let teacherName = prompt("What's your name?")
+
+    alert('Welcome ' + teacherName + '! Please enter the necessary information about your courses.')
+
+}
